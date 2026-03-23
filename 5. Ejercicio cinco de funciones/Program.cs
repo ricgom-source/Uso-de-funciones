5c1c47e480f9eebbf9026bc217b9c5aed1af5571
ricogm-source <ricardoggmenchu@gmail.com>
2026-03-23 14:49:49 -0600

    Agregado de ejercicio cinco de funciones

---
+++ 5. Ejercicio cinco de funciones/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Ejercicio_cinco_de_funciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            void MostrarMenu()
+
+            {
+                Console.WriteLine("Mini Menú de operaciones matemáticas básicas");
+
+                Console.WriteLine();
+
+                Console.WriteLine("1. Sumar");
+
+                Console.WriteLine();
+
+                Console.WriteLine("2. Restar");
+
+                Console.WriteLine();
+
+                Console.WriteLine("3. Salir del menú");
+
+            }
+
+            Console.WriteLine("Presione la tecla enter para desplegar el mini menu");
+
+            Console.ReadKey();
+
+            Console.Clear();
+
+            MostrarMenu();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
