478aec0e6f9f52056f39c1332c87baa08d0af1b7
ricogm-source <ricardoggmenchu@gmail.com>
2026-03-23 15:07:44 -0600

    Agregado de ejercicio siete

---
+++ 7. Ejercicio siete de funciones/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Ejercicio_siete_de_funciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            int MostrarDoble(int n)
+
+            {
+                return n + n;
+
+            }
+
+            Console.Write("Ingrese un numero entero positivo:_ ");
+
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine($"El doble del número ingresado es: {MostrarDoble(num)}");
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
