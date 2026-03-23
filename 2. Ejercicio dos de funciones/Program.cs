3d78096fd3c0fc1cec800692949b49f2816eb9f6
ricogm-source <ricardoggmenchu@gmail.com>
2026-03-23 14:24:42 -0600

    Agregado de ejercicio dos de funciones

---
+++ 2. Ejercicio dos de funciones/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Ejercicio_dos_de_funciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            void MostrarNombre ()
+
+            {
+                Console.WriteLine("Pedro Pérez Pereira");
+
+            }
+
+            Console.WriteLine("Presione enter para visualizar el nombre");
+
+            Console.ReadKey();
+
+            Console.WriteLine();
+
+
+            MostrarNombre();
+
+
+
+
+
+        }
+    }
+}
