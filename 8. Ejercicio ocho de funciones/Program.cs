fe7e3ad9002ba4c242ab6381c674489a9210ff5c
ricogm-source <ricardoggmenchu@gmail.com>
2026-03-23 15:13:41 -0600

    Agregado de ejercicio ocho

---
+++ 8. Ejercicio ocho de funciones/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.Ejercicio_ocho_de_funciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+           
+            int MostrarCuadrado(int n)
+
+            {
+                return n * n;
+
+            }
+
+            Console.Write("Ingrese un número entero positivo:_ ");
+
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine($"El cuadrado de el número ingresado es: {MostrarCuadrado(num)}");
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
