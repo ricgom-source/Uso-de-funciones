e0b2e2cf6e55175a77d7e9161aed586eb260f19b
ricogm-source <ricardoggmenchu@gmail.com>
2026-03-23 14:33:09 -0600

    Agregado de ejercicio cuatro

---
+++ 4. Ejercicio cuatro de funciones/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Ejercicio_cuatro_de_funciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            void Linea()
+
+            {
+                Console.WriteLine("----------------");
+
+            }
+
+            for (int i = 0; i < 10; i++)
+
+            {
+                Console.WriteLine("Hola mundo!");
+
+                Linea();
+
+
+            }
+
+        }
+    }
+}
