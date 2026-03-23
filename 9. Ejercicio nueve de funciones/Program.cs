2da502682e269a9a8d65a5aa849345b827ea9bbd
ricogm-source <ricardoggmenchu@gmail.com>
2026-03-23 15:18:10 -0600

    Agregado de ejercicio nueve de funciones

---
+++ 9. Ejercicio nueve de funciones/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Ejercicio_nueve_de_funciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            int SumarMostrar(int a, int b)
+
+            {
+                return a + b;
+
+
+            }
+
+            Console.Write("Ingrese primer operando:_ ");
+
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("Ingrese segundo operando:_ ");
+
+            int y = int.Parse(Console.ReadLine());  
+
+            Console.WriteLine();
+
+            Console.WriteLine($"El resultado de la suma es:_ {SumarMostrar(x,y)}");
+            
+
+
+
+
+
+
+        }
+    }
+}
