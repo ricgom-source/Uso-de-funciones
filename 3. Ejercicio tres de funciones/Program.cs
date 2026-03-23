987ed31b9638d8e6ed607228461f22bd01cf6e8a
ricogm-source <ricardoggmenchu@gmail.com>
2026-03-23 14:28:24 -0600

    Agregado de ejercicio tres de funciones

---
+++ 3. Ejercicio tres de funciones/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Ejercicio_tres_de_funciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            void MostrarMensaje()
+
+            {
+
+                Console.WriteLine("Programación en C#");
+
+            }
+
+            Console.WriteLine("Preione la tecla enter para desplegar un mesaje");
+
+            Console.ReadKey();
+
+            Console.WriteLine();
+
+            MostrarMensaje();
+
+
+
+
+        }
+    }
+}
---
+++ 3. Ejercicio tres de funciones/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("3. Ejercicio tres de funciones")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("3. Ejercicio tres de funciones")]
+[assembly: AssemblyCopyright("Copyright © HP 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("a3ef0f4e-34d4-4f64-9c7f-77f9df8b7e78")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
